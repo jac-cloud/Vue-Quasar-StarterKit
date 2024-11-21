c63bc946fad2065ecddaa1a8f8ea26652975d64c
Fabio <fabio04musitelli@gmail.com>
2024-11-21 15:10:00 +0100

    fix(missing component)

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,14 @@
 <template>
-  <q-layout>
-    <q-header>
-      <q-toolbar>
-        <q-btn flat dense round icon="menu" @click="$refs.leftDrawer.open()" />
-        <q-toolbar-title>
-          <router-link to="/" class="text-white">Quasar App</router-link>
-        </q-toolbar-title>
-      </q-toolbar>
-    </q-header>
-
-    <q-page-container>
-      <router-view />
-    </q-page-container>
-  </q-layout>
+  <Drawer />
 </template>
 
 <script>
+import Drawer from '/src/components/drawer.vue';
+
 export default {
   name: 'App',
+  components: {
+    Drawer,
+  },
 };
 </script>
--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,8 @@ import App from './App.vue'
 const myApp = createApp(App)
 
 myApp.use(Quasar, {
-  plugins: {}, // import Quasar plugins and add here
+  plugins: {
+  }, // import Quasar plugins and add here
 })
 
 // Assumes you have a <div id="app"></div> in your index.html
